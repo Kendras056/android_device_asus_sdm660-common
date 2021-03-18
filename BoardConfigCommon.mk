3f513120973d5a4112fb2378bc375dbb86b242f5
Dyneteve <sound0020@gmail.com>
2021-03-18 07:06:03 +0600

    sdm660-common: Enable AOSP surfaceflinger
    * AOSP SurfaceFlinger is smoother than CAF
    
    Signed-off-by: Dyneteve <sound0020@gmail.com>
    Change-Id: I1525b9e530df797d3db51d367279fa9861a05581

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -153,6 +153,9 @@ include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
+# SurfaceFlinger
+TARGET_USE_AOSP_SURFACEFLINGER := true
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(COMMON_PATH)
 
