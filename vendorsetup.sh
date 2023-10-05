c98d5ae249ea377435f3cbccec618f691b09444c
Kendras056 <kendras056@gmail.com>
2023-10-05 07:55:28 +0000

    sdm6660-common: Update vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,19 @@
 # Build
 rm -rf build/make
-git clone https://github.com/Tiktodz/android_build build/make
+git clone https://github.com/Kendras056/android_build -b 10 build/make
 
 # KSU
 #rm -rf kernel/asus/sdm660
-git clone --recursive https://github.com/Tiktodz/android_kernel_asus_sdm636 -b 10 kernel/asus/sdm660
+git clone --recursive https://github.com/strongreasons/msm-4.4 -b hmp kernel/asus/sdm660
 
 # LiveDisplay
 rm -rf hardware/lineage/livedisplay
 git clone https://github.com/LineageOS/android_hardware_lineage_livedisplay -b lineage-17.1 hardware/lineage/livedisplay
 
-git clone https://github.com/Tiktodz/device_asus_X00TD -b nad device/asus/X00TD
+# Device
+git clone https://github.com/Kendras056/android_device_asus_X00TD -b 10 device/asus/X00TD
 
-git clone https://github.com/Tiktodz/android_vendor_asus vendor/asus
+# Vendor
+git clone https://github.com/Tiktodz/android_vendor_asus -b lineage-17.1 vendor/asus
 
 cd kernel/asus/sdm660/KernelSU && rm -rf manager docs userspace website scripts && cd ../../../..
