44143ea90307ea0f60c16b143bff100f751904c7
Kendras056 <88943971+Kendras056@users.noreply.github.com>
2023-10-06 11:14:13 +0700

    sdm660-common: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,14 @@
+# Audio
+rm -rf hardware/qcom-caf/msm8998/audio
+git clone https://github.com/AslenoLineageStuff/android_hardware_qcom_audio -b lineage-17.1-caf-msm8998 hardware/qcom-caf/msm8998/audio
+
 # Build
 rm -rf build/make
 git clone https://github.com/Kendras056/android_build -b 10 build/make
 
 # KSU
-#rm -rf kernel/asus/sdm660
-git clone --recursive https://github.com/strongreasons/msm-4.4 -b hmp kernel/asus/sdm660
+rm -rf kernel/asus/sdm660
+git clone --recursive https://github.com/Kendras056/android_kernel_asus_sdm660 -b 10 kernel/asus/sdm660
 
 # LiveDisplay
 rm -rf hardware/lineage/livedisplay
